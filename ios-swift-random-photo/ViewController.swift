7824025db11204ec8eca6cd834d2c91c9da9d3b8
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-09-19 15:07:20 +0530

    Initial Commit

---
+++ ios-swift-random-photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-swift-random-photo
+//
+//  Created by Ashutosh Mohapatra on 19/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
